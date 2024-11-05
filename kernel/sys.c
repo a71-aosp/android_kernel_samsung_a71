2bb7c915471f288da79748475be9da04ac1cbed6
One Smiled <beksultanbazuov@gmail.com>
2024-11-05 15:41:33 +0000

    helo a15 lol

--- kernel/sys.c
+++ kernel/sys.c
@@ -1202,6 +1202,15 @@ SYSCALL_DEFINE1(newuname, struct new_utsname __user *, name)
 
 	down_read(&uts_sem);
 	memcpy(&tmp, utsname(), sizeof(tmp));
+#ifdef CONFIG_UTSNAME_SPOOF	
+	if (!strncmp(current->comm, "netbpfload", 10) &&
+		current->pid != netbpfload_pid) {
+			netbpfload_pid = current->pid;
+			strcpy(tmp.release, "6.6.40");
+			pr_debug("fake_uname: %s/%d release=%s\n",
+				current->comm, current->pid, tmp.release);
+	}
+#endif /* CONFIG_UTSNAME_SPOOF */	
 	up_read(&uts_sem);
 	if (copy_to_user(name, &tmp, sizeof(tmp)))
 		return -EFAULT;
